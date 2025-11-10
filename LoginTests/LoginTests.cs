4edb722d6ba50217b982d1ea1ddf78ec632340f3
romkaCHNU <chub.roman1124@vu.cdu.edu.ua>
2025-11-10 22:07:29 +0200

    Add Paralelisible of testing Settings add try catch patern for test

--- LoginTests/LoginTests.cs
+++ LoginTests/LoginTests.cs
@@ -6,62 +6,79 @@ using POMs;
 
 namespace SauceDemoTests
 {
-    public class LoginTests : IDisposable
+    public class LoginTests
     {
-        private IWebDriver driver;
         [Theory]
         [MemberData(nameof(TestData.Browsers), MemberType = typeof(TestData))]
         public void Login_EmptyCredentials(BrowserType browser)
         {
-            Logger.Info($"[UC1] Starting test on {browser}");
-            driver = DriverManager.GetDriver(browser);
-            var loginPage = new LoginPage(driver);
-            loginPage.Open();
-            loginPage.Login(string.Empty, string.Empty);
-            Logger.Info("Validating error message for empty credentials...");
-            loginPage.GetErrorText().Should().Be("Epic sadface: Username is required");
+            IWebDriver driver = null;
+            try
+            {
+                Logger.Info($"[UC1] Starting test on {browser}");
+                driver = DriverManager.GetDriver(browser);
+                var loginPage = new LoginPage(driver);
+                loginPage.Open();
+                loginPage.Login(string.Empty, string.Empty);
+                Logger.Info("Validating error message for empty credentials...");
+                loginPage.GetErrorText().Should().Be("Epic sadface: Username is required");
+            }
+            finally
+            {
+                DriverManager.Quit();
+            }
         }
 
         [Theory]
         [MemberData(nameof(TestData.Browsers), MemberType = typeof(TestData))]
         public void Login_EmptyPassword(BrowserType browser)
         {
-            Logger.Info($"[UC2] Starting test on {browser}");
-            driver = DriverManager.GetDriver(browser);
-            var loginPage = new LoginPage(driver);
-            loginPage.Open();
-            loginPage.Login("any_user", string.Empty);
-            Logger.Info("Validating error message for empty password...");
-            loginPage.GetErrorText().Should().Be("Epic sadface: Password is required");
+            IWebDriver driver = null;
+            try
+            {
+                Logger.Info($"[UC2] Starting test on {browser}");
+                driver = DriverManager.GetDriver(browser);
+                var loginPage = new LoginPage(driver);
+                loginPage.Open();
+                loginPage.Login("any_user", string.Empty);
+                Logger.Info("Validating error message for empty password...");
+                loginPage.GetErrorText().Should().Be("Epic sadface: Password is required");
+            }
+            finally
+            {
+                DriverManager.Quit();
+            }
         }
 
         [Theory]
         [MemberData(nameof(TestData.ValidCredentials), MemberType = typeof(TestData))]
         public void Login_RightCredentials(string username, string password, bool shouldLogin)
         {
-            Logger.Info($"[UC3] Login test for user: {username}");
-            driver = DriverManager.GetDriver(BrowserType.Edge);
-            var loginPage = new LoginPage(driver);
-            loginPage.Open();
-            loginPage.Login(username, password);
-            if (shouldLogin)
+            IWebDriver driver = null;
+            try
             {
-                Logger.Info($"Validating successful login for {username}");
-                loginPage.AppLogoTxt().Should().Be("Swag Labs");
-                Logger.Info("Then user should be redirected to dashboard.");
+                Logger.Info($"[UC3] Login test for user: {username}");
+                driver = DriverManager.GetDriver(BrowserType.Edge);
+                var loginPage = new LoginPage(driver);
+                loginPage.Open();
+                loginPage.Login(username, password);
+                if (shouldLogin)
+                {
+                    Logger.Info($"Validating successful login for {username}");
+                    loginPage.AppLogoTxt().Should().Be("Swag Labs");
+                    Logger.Info("Then user should be redirected to dashboard.");
+                }
+                else
+                {
+                    Logger.Info($"Validating login failure for {username}");
+                    loginPage.GetErrorText().Should().Be("Epic sadface: Sorry, this user has been locked out.");
+                    Logger.Info("Then user should see an error about being locked out.");
+                }
             }
-            else
+            finally
             {
-                Logger.Info($"Validating login failure for {username}");
-                loginPage.GetErrorText().Should().Be("Epic sadface: Sorry, this user has been locked out.");
-                Logger.Info("Then user should see an error about being locked out.");
+                DriverManager.Quit();
             }
-        }
-
-        public void Dispose()
-        {
-            Logger.Info("Disposing driver");
-            DriverManager.Quit();
         }
     }
 }
